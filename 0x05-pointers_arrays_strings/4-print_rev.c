8a71d3ba670092ef69353c549c21a6f36ed9ddee
teryila <angweteryila@gmail.com>
2023-03-29 06:38:57 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,8 +1,4 @@
 #include "main.h"
-<<<<<<< HEAD
-=======
-
->>>>>>> e8b4987485af40e1747d8d9a0f7cc59c63431e78
 /**
  * print_rev - imprime en reversa
  * @s: string
@@ -12,10 +8,7 @@ void print_rev(char *s)
 {
 	int longi = 0;
 	int o;
-<<<<<<< HEAD
 
-=======
->>>>>>> e8b4987485af40e1747d8d9a0f7cc59c63431e78
 	while (*s != '\0')
 	{
 		longi++;
@@ -27,9 +20,5 @@ void print_rev(char *s)
 		_putchar(*s);
 		s--;
 	}
-<<<<<<< HEAD
-
-=======
->>>>>>> e8b4987485af40e1747d8d9a0f7cc59c63431e78
 	_putchar('\n');
 }
