7ede71ea2aa66050093c0ccb324edf44d20437a3
teryila <angweteryila@gmail.com>
2023-03-29 06:48:24 -0700

    5 commits.

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,23 +5,18 @@
  * @s: Input string
  * Return: String in reverse
  */
-<<<<<<< HEAD
 
-=======
->>>>>>> e8b4987485af40e1747d8d9a0f7cc59c63431e78
 void rev_string(char *s)
 {
 	char rev = s[0];
 	int counter = 0;
 	int i;
-<<<<<<< HEAD
 
 	while (s[counter] != '\0')
 	counter++;
-=======
-	while (s[counter] != '\0')
-		counter++;
->>>>>>> e8b4987485af40e1747d8d9a0f7cc59c63431e78
+while (s[counter] != '\0')
+	counter++;
+
 	for (i = 0; i < counter; i++)
 	{
 		counter--;
