862e04c46429cb38a025dc7bb10745787b758a83
teryila <angweteryila@gmail.com>
2023-03-29 07:10:58 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,25 +9,18 @@
 void print_array(int *a, int n)
 {
 	int i;
-<<<<<<< HEAD
-
-=======
->>>>>>> e8b4987485af40e1747d8d9a0f7cc59c63431e78
-	for (i = 0; i < (n - 1); i++)
+		for (i = 0; i < (n - 1); i++)
 	{
 		printf("%d, ", a[i]);
 	}
-<<<<<<< HEAD
 		if (i == (n - 1))
 		{
 			printf("%d", a[n - 1]);
 		}
 			printf("\n");
-=======
-	if (i == (n - 1))
+		if (i == (n - 1))
 		{
 			printf("%d", a[n - 1]);
 		}
 		printf("\n");
->>>>>>> e8b4987485af40e1747d8d9a0f7cc59c63431e78
 }
