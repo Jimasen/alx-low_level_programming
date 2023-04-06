f83e753c1bf643cfff8a6474c23f6163d8c78ae1
teryila <angweteryila@gmail.com>
2023-04-06 14:27:44 -0700

    main.h

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+int _putchar(char c);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char **argv);
+int main(int argc, char **argv);
+int main(int argc, char **argv);
+int main(int argc, char **argv);
+#endif
+
